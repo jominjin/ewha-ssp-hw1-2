7455a91e76a0c1c7ad352cf6a84db4df1f569eb0
jominjin <jominjin95@naver.com>
2015-03-26 02:04:45 +0900

    Modify cpp file
    Modify the cpp file to change it to Lab Assignment #2-2 in Androx
    Studio.

--- ewha-ssp-hw1-2.cpp
+++ ewha-ssp-hw1-2.cpp
@@ -3,28 +3,37 @@
 
 #define FRONT_LED_PIN 10
 #define REAR_LED_PIN 9
-boolean increasing = true;
-int front_led_value = 0;
+#define DUTY_CYCLE 20 //20ms
+int done = false;
+
+void PWM_Write(int pin, int on_time_perc){
+	int on_time = DUTY_CYCLE*on_time_perc/100.0;
+	int off_time = DUTY_CYCLE - on_time;
+	digitalWrite(pin, HIGH);
+	delay(on_time);
+	digitalWrite(pin, LOW);
+	delay(off_time);
+}
+
+void myAnalogWrite(int pin, int percent, int time){
+	int num_loops = time/DUTY_CYCLE;
+	int i;
+	for (i=0; i<num_loops; i++){
+		PWM_Write(pin, percent);
+	}
+}
 
 void setup(){
-	pinMode(FRONT_LED_PIN, OUTPUT);
 	pinMode(REAR_LED_PIN, OUTPUT);
 }
 
 void loop(){
-	analogWrite(FRONT_LED_PIN, front_led_value);
-	analogWrite(REAR_LED_PIN, 255-front_led_value);
-	delay(20);
-	if (increasing == true)
-		front_led_value++;
-	else
-		front_led_value--;
-	if (front_led_value > 255) {
-		front_led_value = 254;
-		increasing = false;
-	}
-	else if (front_led_value <0) {
-		front_led_value = 1;
-		increasing = true;
-	}
+	if (done == false) {
+		myAnalogWrite(REAR_LED_PIN, 100, 2000);
+		myAnalogWrite(REAR_LED_PIN, 75, 2000);
+		myAnalogWrite(REAR_LED_PIN, 50, 2000);
+		myAnalogWrite(REAR_LED_PIN, 25, 2000);
+		done = true;
+	}else
+		digitalWrite(REAR_LED_PIN, LOW);
 }
